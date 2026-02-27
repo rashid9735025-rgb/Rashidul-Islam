65df07ae2d2409d163f2077bc10845993baa89e3
rashid9735025-rgb <rashid9735025@gmail.com>
2026-02-27 21:16:37 +0530

    Add files via upload

---
+++ age.php
@@ -0,0 +1,16 @@
+{
+"name": "Age Master X",
+"short_name": "AgeX",
+"start_url": "index.html",
+"display": "standalone",
+"background_color": "#0f2027",
+"theme_color": "#764ba2",
+"icons": [
+{
+"src": "icon.png",
+"sizes": "192x192",
+"type": "image/png"
+}
+]
+}
+
